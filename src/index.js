bb89fa450ae03cb47be1001901e9ec5441e7d254
sjayanthi8 <sjayanthi@x.ai>
2026-02-24 21:36:46 -0800

    feat: add version info export and startup message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const message = "Hello World";
+const version = "1.0.0";
 
 function greet() {
   console.log(message);
 }
 
+function getVersion() {
+  return version;
+}
+
+console.log(`Grok Patch v${getVersion()}`);
 greet();
+
+module.exports = {
+  greet,
+  getVersion
+};
